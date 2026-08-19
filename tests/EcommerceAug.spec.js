67c4a092d0cb438ed6a35784276cab0ca65871ed
Surya <suryavk42@gmail.com>
2026-08-19 09:18:34 +0530

    added payment page script

--- tests/EcommerceAug.spec.js
+++ tests/EcommerceAug.spec.js
@@ -1,5 +1,4 @@
 import { test, expect} from "@playwright/test"
-import { text } from "node:stream/consumers";
 
 test ('login' , async ({page}) => {
 
@@ -11,7 +10,7 @@ test ('login' , async ({page}) => {
     await page.locator('#userPassword').fill('Testable@123');
     await page.locator('#login').click();
 
-    await page.screenshot({path : 'screenshot.png'})
+    await page.screenshot({path : 'testScreenshots/login.png'})
     
     //await page.waitForLoadState('networkidle');
     await page.locator('.card-body b').first().waitFor();
@@ -23,8 +22,8 @@ test ('login' , async ({page}) => {
     for(let i=0; i<productCount; i++){
         const productName = await products.nth(i).locator('b').textContent();
 
-        if(productName.trim() == productToSearch){
-            await page.screenshot({path : 'screenshotProduct.png'})
+        if(productName.trim() === productToSearch){
+            await page.screenshot({path : 'testScreenshots/screenshotProduct.png'})
             await products.nth(i).locator('text= Add To Cart ').click();
             break;
         }
@@ -33,7 +32,7 @@ test ('login' , async ({page}) => {
     
     //page.pause();
     const cartBtn = page.locator('[routerlink = "/dashboard/cart"]');
-    await cartBtn.screenshot({path : 'element.png'});
+    await cartBtn.screenshot({path : 'testScreenshots/element.png'});
 
     await page.locator('[routerlink = "/dashboard/cart"]').click();
     await page.locator('.cart h3').waitFor();
@@ -41,7 +40,7 @@ test ('login' , async ({page}) => {
     expect(bool).toBeTruthy();
 
     await page.locator('text=Checkout').click();
-    await page.screenshot({path : 'checkoutPage.png'});
+    await page.screenshot({path : 'testScreenshots/checkoutPage.png'});
 
     //checkout page
 
@@ -49,15 +48,27 @@ test ('login' , async ({page}) => {
     const cc = await page.locator('.payment__type--cc').isEnabled();
     expect(cc).toBeTruthy();
 
-    await page.locator("[class^='input txt']").nth(2).fill('677');
-    await page.locator("[class^='input txt']").nth(3).fill('Surya');
-    await page.locator("[class^='input txt']").nth(4).fill('rahulshettyacademy');
+    await page.locator("[class^='input txt']").nth(1).fill('677');
+    await page.locator("[class^='input txt']").nth(2).fill('Surya');
+    await page.locator("[class^='input txt']").nth(3).fill('rahulshettyacademy');
     await page.locator("button:has-text('Apply Coupon')").click();
-    const coupon = await page.locator('[name="coupon"] + p').isVisible();
+    const coupon = await page.locator('[name="coupon"] + p').isEnabled();
     expect(coupon).toBeTruthy();
 
-    await page.screenshot({path : 'payment.png'});
-    page.pause();
+    await page.locator('[placeholder="Select Country"]').pressSequentially("ind");
+    const country = page.locator('.ta-results');
+    await country.waitFor();
+    const options = await country.locator('button');
+    const optionsCount = await options.count();
+    for(let i = 0; i<optionsCount; i++){
+        const text = await options.nth(i).textContent();
+        if(text === ' India'){
+            await options.nth(i).click();
+            break;
+        }
+    }
+
+    await page.screenshot({path : 'testScreenshots/payment.png'});
 
 
 });
