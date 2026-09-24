e40aff8333d15b9c00fdc5dbbb0269ecffa48569
Surya <suryavk42@gmail.com>
2026-09-24 14:31:48 +0530

    Playwright Login API testcases

--- tests/EcommAPI.spec.js
+++ tests/EcommAPI.spec.js
@@ -1,7 +1,10 @@
 import {test, expect, request} from "@playwright/test";
 
 const loginData = {userEmail: "testable@gmail.com", userPassword: "Testable@123"};
+const orderData = {orders: [{country: "Argentina", productOrderedId: "6960eae1c941646b7a8b3ed3"}]};
 let token;
+let orderID;
+
 test.beforeEach( 'Login using API', async () => {
 //test(async () => {
     const loginAPIContext = await request.newContext();
@@ -23,5 +26,18 @@ test('Login',async ({page}) =>
     }, token);
     await page.pause();
     await page.goto('https://rahulshettyacademy.com/client/#/dashboard/dash');
+
+    const placeOrderContext = await request.newContext();
+   const orderResponse = await placeOrderContext.post('https://rahulshettyacademy.com/api/ecom/order/create-order',
+        {
+            data: orderData,
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'Application/json'
+            },
+        })
+        const orderResponseJSON = await orderResponse.json();
+        orderID = orderResponseJSON.orders;
+        console.log(orderID);
 
 })
